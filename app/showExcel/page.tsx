f1afdcec56048d3965a3f41098415531fe161e56
yousef mostafa <yousefmostafaa121@gmail.com>
2024-10-24 10:10:37 +0300

    add new excel

--- app/showExcel/page.tsx
+++ app/showExcel/page.tsx
@@ -1,7 +1,7 @@
 import PSPDFKitViewer from "@/components/PSPDFKitViewer";
 
 const Page: React.FC = () => {
-  return <PSPDFKitViewer documentPath="/SaleData.xlsx" />;
+  return <PSPDFKitViewer documentPath="/1.4 Sample balance sheet.xls" />;
 };
 
 export default Page;
